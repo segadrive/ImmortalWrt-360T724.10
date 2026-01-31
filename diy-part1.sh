797dbedd5b23afec6d81f2140d0781c7a03e821f
segadrive <47469912+segadrive@users.noreply.github.com>
2026-01-31 09:22:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,7 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git timecontrol https://github.com/sirpdboy/luci-app-timecontrol' >>feeds.conf.default
-echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplanl' >>feeds.conf.default
+# echo 'src-git timecontrol https://github.com/sirpdboy/luci-app-timecontrol' >>feeds.conf.default
+# echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplanl' >>feeds.conf.default
+src-git timecontrol  https://github.com/sirpdboy/luci-app-timecontrol
+src-git taskplan https://github.com/sirpdboy/luci-app-taskplanl
